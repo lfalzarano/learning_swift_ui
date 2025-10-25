d34f432d6fece7838dadae4fa4179aae4432c90b
loganfalzarano <lfalzar1@jhu.edu>
2025-10-25 11:51:34 -0700

    Day 27

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Logan Falzarano on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
