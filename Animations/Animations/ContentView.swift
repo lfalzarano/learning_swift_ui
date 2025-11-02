477928585fea0a68a41c25cf41e45fac0c826def
loganfalzarano <lfalzar1@jhu.edu>
2025-11-02 15:33:09 -0800

    Day 33)

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Logan Falzarano on 10/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let letters = Array("Hello Roxy")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+                     
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
+    }
+}
+
+#Preview {
+    ContentView()
+}
