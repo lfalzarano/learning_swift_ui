a45601c76225bf2bb0144d126b996ec88d473bb2
loganfalzarano <lfalzar1@jhu.edu>
2025-11-15 19:56:15 -0800

    Day 43

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Logan Falzarano on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
