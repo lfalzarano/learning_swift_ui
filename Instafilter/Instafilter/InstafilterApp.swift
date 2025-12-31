ea88e163463420ceedeead68ba5c1264853c7d1d
loganfalzarano <lfalzar1@jhu.edu>
2025-12-30 20:49:41 -0500

    day 62

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Logan Falzarano on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
